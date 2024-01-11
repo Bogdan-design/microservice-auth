ab00aaf20375df8a044d269d32a4bc9853973ed4
Bogdan-design <bogdanbw@gmail.com>
2024-01-11 10:33:54 +0100

    main page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <>
+            dashboard page
+        </>
+    )
+}
+
+export default Dashboard
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className='font-semibold text-green-500'>Hello Auth</p>
+    <Button size='lg'>Click me</Button>
   )
 }
