e3afdf5a53eaf1173bf7c0f583682d68cb64d77f
Bogdan-design <bogdanbw@gmail.com>
2024-01-12 20:31:08 +0100

    auth page

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React, {ReactNode} from 'react';
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
+
+type CardWrapperProps = {
+    children: ReactNode
+    headerLabel: string
+    backButtonLabel: string
+    backButtonHref: string
+    showSocial?: boolean
+}
+
+
+export const CardWrapper = ({
+                                children,
+                                backButtonHref,
+                                backButtonLabel,
+                                headerLabel,showSocial
+}:CardWrapperProps) => {
+    return (
+        <Card className=''>
+            {children}
+        </Card>
+    );
+};
