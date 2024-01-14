bbe247ede04407d81e611ae877fe708fcbf69047
Bogdan-design <bogdanbw@gmail.com>
2024-01-14 21:54:30 +0100

    header component

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {CardWrapper} from "@/components/auth/card-wrapper";
 
 export const LoginForm = () => {
     return (
-        <div>
+        <CardWrapper headerLabel='Walcom back'
+        backButtonLabel='Don`t have an accont?'
+                     backButtonHref='/auth/register'
+                     showSocial
+        >
 Login Form!
-        </div>
+        </CardWrapper>
     );
 };
